30d6becae6081b8a434d17deb340a6a6899ec5bc
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-08-24 19:10:32 +0200

    Cosmetic changes

--- exercise-3-sol.py
+++ exercise-3-sol.py
@@ -15,14 +15,24 @@ Y = [75,75,78,79,80,81,82]
 fig = plt.figure(figsize=(10,6))
 ax = fig.add_subplot(111)
 
+for y in np.arange(0,101,20):
+    ax.axhline(y,color='.75', zorder=-100)
+
 plt.scatter(X, Y, s=100, color='white', linewidth=2.5, edgecolor='blue', zorder=10)
-plt.plot(X,Y, color='blue', linewidth=2.5)
+plt.plot(X,Y, color='blue', linewidth=2.5, zorder=0)
 
 plt.xlim(2006,2014)
 plt.xticks(X, ['%d' % x for x in X])
 
 plt.ylim(0,100)
 plt.yticks(np.arange(0,101,20), ["%d%%" % x for x in np.arange(0,101,20)])
+
+
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
 
 plt.savefig("exercise-3-sol.png")
 plt.show()
